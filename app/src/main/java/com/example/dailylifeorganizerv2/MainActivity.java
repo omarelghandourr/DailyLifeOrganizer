f6a3662b8d621a2eea03f0cbd2f849b41407a23e
omare <omarelghandour@outlook.com>
2026-06-18 17:03:33 +0200

    Implemented splash screen and home screen navigation

--- app/src/main/java/com/example/dailylifeorganizerv2/MainActivity.java
+++ app/src/main/java/com/example/dailylifeorganizerv2/MainActivity.java
@@ -1,24 +1,38 @@
 package com.example.dailylifeorganizerv2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Button btnNotes = findViewById(R.id.btnNotes);
+        Button btnTodo = findViewById(R.id.btnTodo);
+        Button btnDates = findViewById(R.id.btnDates);
+        Button btnShopping = findViewById(R.id.btnShopping);
+        Button btnAbout = findViewById(R.id.btnAbout);
+
+        btnNotes.setOnClickListener(v ->
+                startActivity(new Intent(this, NotesActivity.class)));
+
+        btnTodo.setOnClickListener(v ->
+                startActivity(new Intent(this, TodoActivity.class)));
+
+        btnDates.setOnClickListener(v ->
+                startActivity(new Intent(this, DatesActivity.class)));
+
+        btnShopping.setOnClickListener(v ->
+                startActivity(new Intent(this, ShoppingActivity.class)));
+
+        btnAbout.setOnClickListener(v ->
+                startActivity(new Intent(this, AboutActivity.class)));
     }
 }
---
+++ app/src/main/java/com/example/dailylifeorganizerv2/ShoppingActivity.java
@@ -0,0 +1,4 @@
+package com.example.dailylifeorganizerv2;
+
+public class ShoppingActivity {
+}
